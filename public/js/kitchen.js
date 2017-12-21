5988f509798c6ef801cebf35b9eae52e0ce73ea7
Anna <you@example.com>
2017-12-21 11:23:43 +0100

    har fixat med next order to prepare i staff view

--- public/js/kitchen.js
+++ public/js/kitchen.js
@@ -24,14 +24,11 @@ Vue.component('order-item-to-prepare', {
       this.$emit('done');
     },
     cancelOrder: function () {
-      console.log("cancel order");  
-      var r=confirm("You are cancelling an order \nEither OK or Cancel.");
-      if (r == true) {
       this.$emit('cancel');
      } 
 }
     
-    }    
+       
 });
 
 
@@ -121,5 +118,23 @@ var vm = new Vue({
   }
 });
 
+/*var slider = document.getElementById('slider-color');
+
+noUiSlider.create(slider, {
+	start: [ 4000, 8000, 12000, 16000 ],
+	connect: [false, true, true, true, true],
+	range: {
+		'min': [  2000 ],
+		'max': [ 20000 ]
+	}
+});
+
+var connect = slider.querySelectorAll('.noUi-connect');
+var classes = ['c-1-color', 'c-2-color', 'c-3-color', 'c-4-color', 'c-5-color'];
+
+for ( var i = 0; i < connect.length; i++ ) {
+    connect[i].classList.add(classes[i]);
+}
+*/
 
     
