2a0e0f3a2ffb40757cf31d8238dc18b301cacc19
Anna <you@example.com>
2017-12-22 13:55:01 +0100

    slider fixad. förhoppningsvis ska det inte bli fel i npm nu.

--- public/js/kitchen.js
+++ public/js/kitchen.js
@@ -124,7 +124,7 @@ noUiSlider.create(slider, {
 var connect = slider.querySelectorAll('.noUi-connect');
 var classes = ['c-1-color', 'c-2-color', 'c-3-color', 'c-4-color', 'c-5-color'];
 
-for ( vari = 0; i < connect.length; i++ ) {
+for ( var i = 0; i < connect.length; i++ ) {
     connect[i].classList.add(classes[i]);
 }
 
