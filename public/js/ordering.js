6ea15fca9978ad17ae69b2854e73f9778cab3d26
Anna <you@example.com>
2018-01-07 12:38:39 +0100

    färgruta vid your order

--- public/js/ordering.js
+++ public/js/ordering.js
@@ -450,9 +450,9 @@ var vm = new Vue({
             });
 
             var connect = this.slider.querySelectorAll('.noUi-connect');
-            var classes = ['c-1-color', 'c-2-color', 'c-3-color', 'c-4-color', 'c-5-color'];
-
-            for (var i = 0; i < connect.length; i++) {
+           // var classes = ['c-1-color', 'c-2-color', 'c-3-color', 'c-4-color', 'c-5-color'];
+//baklänges för att få färgerna i rätt ordning i koppen
+            for (var i = 0; i<connect.length; i++) {
                 connect[i].style.background=this.colors[i];
             }
 
